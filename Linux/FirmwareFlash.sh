ee344a8280d50fce022543fa11566b066a014c15
PrimaryComet <christopher.blum@manchester-robotics.com>
2022-03-04 11:22:14 +0000

    Fix Linux script

--- Linux/FirmwareFlash.sh
+++ Linux/FirmwareFlash.sh
@@ -1,3 +1,9 @@
-python esptool.py --chip esp32 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ../bin/boot_app0.bin 0x1000 ../bin/bootloader_qio_80m.bin 0x10000 ../bin/MainSrc.ino.bin 0x8000 ../bin/MainSrc.ino.partitions.bin
+for port in ls -1 /dev/tty*
+do
+	if [[ $port == *"USB"* ]]; then
 
-python esptool.py --chip esp32 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x00290000 ../bin/MainSrc.spiffs.bin 
+		python esptool.py --port $port --chip esp32 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ../bin/boot_app0.bin 0x1000 ../bin/bootloader_qio_80m.bin 0x10000 ../bin/MainSrc.ino.bin 0x8000 ../bin/MainSrc.ino.partitions.bin
+
+		python esptool.py --port $port --chip esp32 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x00290000 ../bin/MainSrc.spiffs.bin 
+	fi
+done  
--- Windows_Batch/build/readme.txt
+++
@@ -1,4 +0,0 @@
-To generate .exe from python file (requires pyinstaller and python):
-rmdir /s /q build_tmp
-mkdir build_tmp
-<path>\pyinstaller.exe --onefile --specpath build_tmp --workpath build_tmp\build --distpath build_tmp\dist <path>\edit_config.py
